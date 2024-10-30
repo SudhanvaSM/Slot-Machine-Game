c2b746bc4b1c21cdae56403ed1eeb6851d4c8917
Sudhanva S M <sudhanva.madhusudhan@gmail.com>
2024-10-30 22:30:02 +0530

    slot_machine_game.py

--- slot_machine_game.py
+++ slot_machine_game.py
@@ -1,21 +1,21 @@
-import random
+import random                #for randomising the slot spin values
 
-MAX_LINES = 3
-MAX_BET = 100
-MIN_BET = 1
+MAX_LINES = 3                #number of lines per spin
+MAX_BET = 100                #max amount of bet a user can place on all the lines combined
+MIN_BET = 1                  #min amount of bet a user can place
 
-ROWS = 3
+ROWS = 3                    
 COLS = 3
 
-symbol_count = {
+symbol_count = {        #frequency of each symbol that can appear in the spin
     '🍒' : 2,
     '🍉' : 3,
     '🍋' : 4,
     '🔔' : 5,
     '⭐' : 4,
 }
-
-symbol_value = {
+    
+symbol_value = {        #value of each symbol, higher the frequency lower is the value in final winnings
     '🍒' : 5,
     '🍉' : 4,
     '🍋' : 3,
@@ -24,17 +24,17 @@ symbol_value = {
 }
 
 def get_slot_spin(rows,cols,symbols):
-    all_symbols = []
+    all_symbols = []                                        #repeats each symbol specified in symbol_count.
     for symbol, symbol_count in symbols.items():
         for _ in range (symbol_count):
             all_symbols.append(symbol)
 
-    copy_symbols = all_symbols[:]
+    copy_symbols = all_symbols[:]                           #to use for random selection without modifying the original list
     columns = []
     for _ in range (cols):
         column = []
         for _ in range (rows):
-            value = random.choice(copy_symbols)
+            value = random.choice(copy_symbols)            #randomly selects symbols from copy_symbolsand appends the selected symbol to the current column
             copy_symbols.remove(value)
             column.append(value)
         
@@ -42,7 +42,7 @@ def get_slot_spin(rows,cols,symbols):
 
     return (columns)
 
-def print_columns(columns):
+def print_columns(columns):                                #formats and displays the symbols in 3x3 matrix form
     for row in range (len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
@@ -52,7 +52,7 @@ def print_columns(columns):
         
         print()
         
-def check_winnings(columns,lines,bet,values):
+def check_winnings(columns,lines,bet,values):            #checks each line if all the symbols are equal
     winnings = 0
     winning_lines = []
     for line in range(lines):
@@ -62,12 +62,12 @@ def check_winnings(columns,lines,bet,values):
             if symbol != symbol_to_check:
                 break
         else:
-            winnings += values[symbol] * bet
+            winnings += values[symbol] * bet            #multiply original bet amount with values assigined in symbol_value
             winning_lines.append(line + 1)
     return winnings, winning_lines
 
 
-def deposit():
+def deposit():                                          #asks the user to deposit a valid amount
     while True:
         amount = input("How much do you want to deposit? $")
         if amount.isdigit():
@@ -81,7 +81,7 @@ def deposit():
         
     return amount
 
-def get_lines():
+def get_lines():                                        #asks the user how many lines they want to bet on
     while True:
         lines = input(f"How many lines would you like to bet on (1-{MAX_LINES}): ")
         if lines.isdigit():
@@ -95,7 +95,7 @@ def get_lines():
         
     return lines
     
-def get_bet():
+def get_bet():                                        #asks the user the value of each line they want to bet on
     while True:
         bet = input("How much do you want to bet? $")
         if bet.isdigit():
@@ -109,7 +109,7 @@ def get_bet():
         
     return bet
 
-def game(balance):
+def game(balance):                                #deducts the required amount from deposit 
     lines = get_lines()
     while True:
         bet = get_bet()
